dfbb2a01dbb681bedd7f8195c3e59fbd44abbf40
mohammad <mohammad@inpinapp.com>
2018-11-03 13:25:15 +0330

    Create Messages class.

---
+++ src/Utils/Messages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Inpin\Foundation\Utils;
+
+
+class Messages
+{
+    const RESPONSE_DESTROYED = 'Object has been deleted successfully.';
+    const RESPONSE_RESTORED = 'Object has been restored successfully.';
+    const RESPONSE_NOT_DESTROYED = 'Object has not been deleted successfully.';
+}
