c2f5ae310cda7e2c1feb9439425087d22f0bb21c
esmaeel <esmaeel@inpinapp.com>
2018-11-12 14:38:52 +0330

    Create Helper.php file

---
+++ src/Utils/Helper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Inpin\Foundation\Utils;
+
+
+class Helper
+{
+    /**
+     * Append given prefix to given array's keys.
+     *
+     * @param array $array
+     * @param $prefix
+     * @return array
+     */
+    public static function appendPrefixToArrayKeys(array $array, $prefix)
+    {
+        foreach ($array as $key => $value) {
+            $array[$prefix . $key] = $value;
+            unset($array[$key]);
+        }
+
+        return $array;
+    }
+
+    /**
+     * Append given parent name to the the given array.
+     *
+     * @param array $array
+     * @param $parent
+     * @return array
+     */
+    public static function appendParentToArrayKeys(array $array, $parent)
+    {
+        if ($parent == '') {
+            return $array;
+        }
+
+        return self::appendPrefixToArrayKeys($array, $parent . '.');
+    }
+}
